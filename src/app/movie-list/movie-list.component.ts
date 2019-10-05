4923223970274996d075c6683f53eeb06cacdfcf
emrejz <emre.kayacan@msn.com>
2019-10-05 22:58:21 +0300

    Load more results after scroll to end ok

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,5 +1,6 @@
 import { MovieService } from "./../services/movie/movie.service";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, HostListener } from "@angular/core";
+import { MovieInList } from "../models/movieInList";
 
 @Component({
   selector: "app-movie-list",
@@ -8,9 +9,29 @@ import { Component, OnInit } from "@angular/core";
 })
 export class MovieListComponent implements OnInit {
   constructor(private movieService: MovieService) {}
-
+  movies: MovieInList[] = [];
+  tryLoad: boolean = true;
   ngOnInit() {}
+  @HostListener("window:scroll", ["$event"])
+  listeningScroll(event) {
+    let movieListTag = document.getElementById("movieList");
+    console.log(movieListTag.offsetHeight);
+    console.log("window.innerHeight", window.innerHeight);
+    console.log("window.window.scrollY", window.window.scrollY);
+    console.log(window.innerHeight + window.scrollY);
+
+    if (
+      window.innerHeight + window.scrollY >= movieListTag.offsetHeight &&
+      this.tryLoad
+    ) {
+      this.getMovies();
+    }
+  }
   getMovies() {
-    this.movieService.getMovies().subscribe(data => console.log(data));
+    this.tryLoad = false;
+    this.movieService.getMovies().subscribe(data => {
+      this.tryLoad = true;
+      this.movies.push(...data["Search"]);
+    });
   }
 }
